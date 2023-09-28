782ac35ec5114d89acc4371ac4cd11ecaf2315ca
vijay-seetharam3 <seetharamvijay3@gmail.com>
2023-09-28 21:29:40 +0530

    big number of all

---
+++ biggestofall.py
@@ -0,0 +1,9 @@
+a = int (input("enter 1st number"))
+b = int (input("enter 2nd number"))
+c = int (input("enter 3rd number"))
+if(a>b and a>c):
+    print(a,"is bigger")
+elif(b>c):
+    print(b,"is bigger")
+else:
+    print(c,"is bigger")
